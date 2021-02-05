dfcddb1c7ab92044b1c103faa02b2faa7bac5415
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-05 17:11:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,6 @@ resource "google_container_cluster" "default" {
   remove_default_node_pool = true
   initial_node_count       = var.initial_node_count
 
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
 }
 
 resource "google_container_node_pool" "default" {
