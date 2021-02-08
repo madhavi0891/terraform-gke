919ce9719fff4d04f2f64348f91ec51f2adff917
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-08 18:28:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ resource "google_container_cluster" "default" {
 
   remove_default_node_pool = true
   initial_node_count       = var.initial_node_count
+  
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
 
 }
 
@@ -15,9 +18,7 @@ resource "google_container_node_pool" "default" {
   location   = var.location
   cluster    = google_container_cluster.default.name
   node_count = 1
-  
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
+
 
   node_config {
     preemptible  = true
