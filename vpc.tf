bfefa14fcf1abba5bf657b69042642f53df9fe22
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-08 18:34:22 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project}-subnet"
+  region        = var.location
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
