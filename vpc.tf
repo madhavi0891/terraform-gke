a294288eb2948e16dffbcc03ce4122f66546c305
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-11 16:58:23 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,4 +8,4 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project}-subnet"
   region        = var.location
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = "0.0.0.0/0"
